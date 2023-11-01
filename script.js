493354807a8cc457cf0814b3a4e50f69103dc3df
HeoMunMun <142040911+HeoMunMun@users.noreply.github.com>
2023-11-01 13:42:43 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+// Lựa chọn và sắp xếp danh sách "books"
+const books = document.querySelector('#books');
+const bookListItems = Array.from(books.querySelectorAll('li'));
+
+bookListItems.sort((a, b) => {
+  if (a.textContent < b.textContent) {
+    return 1;
+  } else if (a.textContent > b.textContent) {
+    return -1;
+  } else {
+    return 0;
+  }
+});
+
+bookListItems.forEach((item) => {
+  books.appendChild(item);
+});
+
+// Lựa chọn và sắp xếp danh sách "movie"
+const movies = document.querySelector('#movie');
+const movieListItems = Array.from(movies.querySelectorAll('li'));
+
+movieListItems.sort((a, b) => {
+  if (a.textContent < b.textContent) {
+    return 1;
+  } else if (a.textContent > b.textContent) {
+    return -1;
+  } else {
+    return 0;
+  }
+});
+
+movieListItems.forEach((item) => {
+  movies.appendChild(item);
+});
+
+// Lựa chọn và sắp xếp danh sách "soft"
+const softwares = document.querySelector('#soft');
+const softListItems = Array.from(softwares.querySelectorAll('li'));
+
+softListItems.sort((a, b) => {
+  if (a.textContent < b.textContent) {
+    return 1;
+  } else if (a.textContent > b.textContent) {
+    return -1;
+  } else {
+    return 0;
+  }
+});
+
+softListItems.forEach((item) => {
+  softwares.appendChild(item);
+});
