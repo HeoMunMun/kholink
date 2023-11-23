04a98e8869a6a45c625f8a2f92550ff935fe56c9
HeoMunMun <142040911+HeoMunMun@users.noreply.github.com>
2023-11-23 09:49:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -33,3 +33,23 @@ movieListItems.sort((a, b) => {
 movieListItems.forEach((item) => {
   movies.appendChild(item);
 });
+
+// Lấy danh sách tất cả các thẻ a trong ul
+    var links = document.querySelectorAll('#linkList a');
+
+    // Duyệt qua từng thẻ a và thêm sự kiện click
+    links.forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            // Ngăn chặn hành động mặc định của thẻ a (mở trang mới)
+            event.preventDefault();
+
+            // Lấy href của thẻ a được click
+            var originalLink = link.getAttribute('href');
+
+            // Tạo đường link mới với token và đích đến mong muốn
+            var newLink = 'https://web1s.com/st?token=cd653021-e55d-475b-b96b-247b5f38932d&url=' + encodeURIComponent(originalLink);
+
+            // Mở tab mới với đường link mới
+            window.open(newLink, '_blank');
+        });
+    });
